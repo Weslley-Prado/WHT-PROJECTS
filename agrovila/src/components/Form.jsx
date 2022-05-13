d423ea675d454cfb84a9dc763d85a66e4d6b159c
Weslley Prado <weslley.prado73@gmail.com>
2022-05-13 19:34:13 -0300

    Form update

--- agrovila/src/components/Form.jsx
+++ agrovila/src/components/Form.jsx
@@ -44,27 +44,33 @@ export default class Form extends Component {
                 <h3 className='contactTitulo'> Deixe sua mensagem aqui</h3>
                 <div>
                     <form action="/action_page.php">
-                        <label>First Name</label>
                         <input type="text" id="fname" name="firstname" placeholder="Your name.."
                             value={this.state.fname}
                             onChange={e => this.setState({ fname: e.target.value })} />
-                        <label>Last Name</label>
                         <input type="text" id="lname" name="lastname" placeholder="Your last name.."
                             value={this.state.lname}
                             onChange={e => this.setState({ lname: e.target.value })} />
 
 
-                        <label>Email</label>
                         <input type="email" id="email" name="email" placeholder="Your email"
                             value={this.state.email}
                             onChange={e => this.setState({ email: e.target.value })} />
 
+                        <input type="text" id="phone" name="phone" placeholder="Seu telefone"
+                            value={this.state.phone}
+                            onChange={e => this.setState({ phone: e.target.value })} />
 
-                        <label>Subject</label>
+                        <input type="text" id="estado" name="estado" placeholder="Seu estado"
+                            value={this.state.estado}
+                            onChange={e => this.setState({ estado: e.target.value })} />
+                         <input type="text" id="cidade" name="cidade" placeholder="Sua cidade"
+                            value={this.state.cidade}
+                            onChange={e => this.setState({ cidade: e.target.value })} />
+                        
                         <textarea id="subject" name="subject" placeholder="Write something.."
                             onChange={e => this.setState({ message: e.target.value })}
                             value={this.state.message}></textarea>
-                        <input type="submit" onClick={e => this.handleFormSubmit(e)} value="Submit" />
+                        <input className="send"type="submit" onClick={e => this.handleFormSubmit(e)} value="Submit" />
                         <div>
                             {this.state.mailSent &&
                                 <div>Thank you for contcting us.</div>
