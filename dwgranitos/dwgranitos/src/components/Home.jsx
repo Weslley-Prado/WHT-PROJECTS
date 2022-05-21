7bd823cd6102c1281501643ee13d99a5ee1a3fdd
Weslley Prado <weslley.prado73@gmail.com>
2022-05-20 22:27:17 -0300

    Update slide

--- dwgranitos/dwgranitos/src/components/Home.jsx
+++ dwgranitos/dwgranitos/src/components/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import image1 from '../../src/img/banner.png';
+
 import image2 from '../../src/img/banner2.png';
 import image3 from '../../src/img/banner3.png';
 import image4 from '../../src/img/banner2.png';
@@ -14,6 +16,7 @@ const Home = () => {
                 <section>
                 <div className="sec__one">                       
                       <AliceCarousel className="slide" autoPlay autoPlayInterval="3000">
+                                <img src={image1} className="sliderimg" alt=""/>
                                 <img src={image2} className="sliderimg" alt=""/>
                                 <img src={image3} className="sliderimg" alt="" />
                                 <img src={image4} className="sliderimg" alt=""/>
@@ -21,6 +24,16 @@ const Home = () => {
                 </div>
                 <h2 className="order">FAÇA SEU ORÇAMENTO</h2>
                 <Form /> 
+                <div>
+                    <h2 className="order">Por que escolher a DW Granitos?</h2>
+                    <div>
+                            <p className="paragrafo lead">
+                            A DW está no mercado como compromisso de atender residências, comércios,
+indústrias, prédios, condomínios, construtoras, decoradores, arquitetos e engenheiros,
+com produtose serviços da mais alta qualidade. </p>
+                            
+                    </div>
+                </div>
                 </section>
         )
 }
