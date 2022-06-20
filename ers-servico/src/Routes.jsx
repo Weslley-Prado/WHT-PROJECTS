4f0982e4a320f541c4bfb74f196139e0cec506f6
Laísa Cominotti <weslley.prado73@gmail.com>
2022-06-19 22:58:33 -0300

    New page, services

--- ers-servico/src/Routes.jsx
+++ ers-servico/src/Routes.jsx
@@ -5,6 +5,7 @@ import Home from "./components/home/Home";
 import QuemSomos from "./components/quem-somos/Quem-somos";
 import Orcamento from "./components/orcamento/Orcamento";
 import TrabalheConosco from "./components/trabalhe-conosco/Trabalhe-conosco";
+import Servicos from "./components/servico/Servicos";
 
 
 
@@ -14,7 +15,8 @@ export default props => (
 
     <Routes>
         <Route path='/' element={<Home />}/>
-        <Route path='/quem-somos' element={<QuemSomos />}/> 
+        <Route path='/quem-somos' element={<QuemSomos />}/>
+        <Route path='/servicos' element={<Servicos />}/> 
         <Route path='/orcamento' element={<Orcamento />}/>      
         <Route path='/trabalhe-conosco' element={<TrabalheConosco />}/>      
 
